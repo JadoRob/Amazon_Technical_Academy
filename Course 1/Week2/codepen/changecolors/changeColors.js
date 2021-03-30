b8fa7f0536b09b89c8262b7a9a8c6597ed8189ef
JadoRob <renojado@gmail.com>
2021-03-30 11:01:17 -0400

    Exercise using javascript to change background color of HTML div

---
+++ Course 1/Week2/codepen/changecolors/changeColors.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    var divElement1 = document.getElementById("div1");
+    var divElement2 = document.getElementById("div2");
+    divElement1.className = "blueback";
+    divElement2.className = "orangeback";
+  }
--- Course 1/Week2/modifyingImages.js
+++ Course 1/Week2/modifyingImages.js
@@ -1,5 +1,6 @@
-//Programming Exercise: Modifying Images
-//Part 1
+//------------------------------------------------------------------------
+// Programming Exercise: Modifying Images
+// Part 1
 var picture = new SimpleImage("astrachan.jpg");
 var width = picture.getWidth();
 for (var pixel of picture.values()) {
@@ -10,6 +11,36 @@ for (var pixel of picture.values()) {
         pixel.setGreen(255);
     } else {
         pixel.setBlue(255);
+    }
+}
+print(picture);
+
+//------------------------------------------------------------------------
+// Part 2
+// Swap Red and Grreen
+var picture = new SimpleImage("drewRobert.png");
+print(picture);
+for (var pixel of picture.values()) {
+    var swap = pixel.getRed();
+    pixel.setRed(pixel.getGreen());
+    pixel.setGreen(swap);
+}
+print(picture);
+
+//------------------------------------------------------------------------
+// Part 3
+// Change the Duke blue devil
+var picture = new SimpleImage("duke_blue_devil.png");
+print(picture);
+for (var pixel of picture.values()) {
+    if (pixel.getBlue() >= 227) {
+        if (pixel.getRed() != 255) {
+            if (pixel.getGreen() != 255) {
+                pixel.setBlue(0);
+                pixel.setRed(255);
+                pixel.setGreen(255);
+            }
+        }
     }
 }
 print(picture);
